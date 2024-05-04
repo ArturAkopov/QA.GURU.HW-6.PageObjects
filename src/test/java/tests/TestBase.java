97803a4850a21fceb30d1fd0855f8cbce6a790ef
Artur <116442638+ArturAkopov@users.noreply.github.com>
2024-05-04 19:44:54 +0300

    Косметика

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,15 +5,12 @@ import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
-
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
 
     @AfterEach
